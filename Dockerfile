8bf970ae6fd76f54d3f056c77bca58d497f1bdf7
Daniel Firebanks-Quevedo <dfirebanks@ibm.com>
2021-04-09 11:56:27 -0400

    Updated Dockerfile permissions and simplified app

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,13 @@ COPY . /app
 
 RUN pip3 --no-cache-dir install -r requirements.txt
 
+ENV APP_ROOT=/app
+RUN true \
+    && chmod -R u+x ${APP_ROOT} \
+    && chgrp -R 0 ${APP_ROOT} \
+    && chmod -R g=u ${APP_ROOT} \
+    && true
+
 EXPOSE 5000
 
 ENTRYPOINT ["python3"]
--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,23 @@
 from flask import Flask, request, Response
+import json
 
 app = Flask(__name__)
+
 
 @app.route('/webhook', methods=['POST'])
 def respond():
     print(request.json)
+    with open("test_webhook.json", "w") as f:
+        json.dump(request.json, f)
     return Response(status=200)
+
 
 @app.route("/index")
 def index():
+    with open("test_index.json", "w") as f:
+        json.dump({"bro": "dis json"}, f)
     return "Index Hello World!"
+
 
 @app.route("/")
 def main():
@@ -18,4 +26,5 @@ def main():
 
 if __name__ == '__main__':  # Script executed directly?
     print("Hello World! Built with a Docker file.")
-    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)  # Launch built-in web server and run this Flask webapp
+    app.run(host="0.0.0.0", port=5000, debug=True,
+            use_reloader=True)  # Launch built-in web server and run this Flask webapp
